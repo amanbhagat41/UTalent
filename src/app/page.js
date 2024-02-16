8ea076c66172bdf428325c816525b9a9163056c4
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2024-02-15 19:25:43 -0500

    footer added

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,14 @@ import logo from "../../public/images/logo-no-bg.png";
 import { CarouselDemo } from "@/components/carousel";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { NavigationMenuDemoFooter } from "@/components/navfooter";
 export default function Page() {
   return (
     <>
       <nav className="bg-error-100 h-20 sticky top-0">
         <div className="flex items-center justify-between h-full">
           <div>
-            <Image src={logo} width="150" height="150"></Image>
+            <Image src={logo} width="150" height="150" alt="logo"></Image>
           </div>
           <div className="flex justify-end flex-grow">
             <NavigationMenuDemo />
@@ -37,6 +38,17 @@ export default function Page() {
       </div>
       <div className="pt-14 flex items-center justify-center">
         <CarouselDemo></CarouselDemo>
+      </div>
+      <div>
+        <footer className="bg-error-100 h-20 w-full absolute left-0 bottom-0">
+        <div className="flex items-center justify-between h-full">
+          <h4 className="text-2xl text-error-200 ml-8">UTalent</h4>
+          {/* <h4 className="flex justify-end flex-grow"></h4> */}
+          <div className="flex justify-end flex-grow mr-10  ">
+          <NavigationMenuDemoFooter ></NavigationMenuDemoFooter>
+          </div>
+          </div>
+        </footer>
       </div>
     </>
   );
