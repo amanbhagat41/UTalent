df3ff69775384ca3893667a50c2f867e2b4830aa
Michael-Chagnon <michaelchagnon12@gmail.com>
2024-02-22 14:54:35 -0500

    image fixed

--- src/app/jobPostingEmployer/page.js
+++ src/app/jobPostingEmployer/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationMenuDemo } from "@/components/navJobPostingEmployer";
 import Image from "next/image";
-// import logo from "../../public/images/logo-no-bg.png";
+import logo from "../../../public/images/logo-no-bg.png";
 
 export default function jobPostingEmployer() {
   return (
@@ -10,7 +10,7 @@ export default function jobPostingEmployer() {
       <nav className="bg-error-100 h-20 sticky top-0 z-40 dark:bg-error-black">
         <div className="flex items-center justify-between h-full">
           <div>
-            {/* <Image src={logo} width="150" height="150" alt="logo"></Image> */}
+            <Image src={logo} width="150" height="150" alt="logo"></Image>
           </div>
           <div className="flex justify-end flex-grow ">
             <NavigationMenuDemo />
