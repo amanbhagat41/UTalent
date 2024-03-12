bdb890ce73252a8ed482fe706b70fc81b2bfd85d
Alex Kashian 01895339 <askashian@gmail.com>
2024-03-11 21:35:13 -0400

    bookmark button
    still needs to change backgroud color once clicked on

--- src/components/job-posting/jobPostingCard.js
+++ src/components/job-posting/jobPostingCard.js
@@ -14,12 +14,23 @@ import Link from "next/link";
 function JobPostingCard({ job, index }) {
     return (
         <Card className="w-[420px] h-[420px] dark:border-error-white dark:shadow-glow dark:bg-error-black rounded-3xl">
-            <img
-                src="images/jobPostIcon.png"
-                alt=""
-                style={{ width: "48px", height: "48px" }}
-                className="justify-start mt-2 ml-5 bg-transparent rounded-3xl"
-            />
+            <div class="flex items-center justify-between">
+                <div>
+                    <img
+                        src="images/jobPostIcon.png"
+                        alt=""
+                        style={{ width: "48px", height: "48px" }}
+                        className="justify-start mt-2 ml-5 bg-transparent rounded-3xl"
+                    />
+                </div>
+                <div class = "flex justify-end flex-grow pr-4">
+                    <button class="bg-white mt-2 p-3 font-bold rounded-lg border inline-flex items-center hover:bg-accent hover:text-accent-foreground focus:bg-violet-300">
+                        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"></path></svg>
+                        <span>Bookmark</span>
+                    </button>
+                </div>
+            </div>
             <CardHeader className="-mt-5">
                 <div className="flex justify-between items-start">
                     <div>
