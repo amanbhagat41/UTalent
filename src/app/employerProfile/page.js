ac414652e989bf155aae8e24dd9b86c5dafea3e2
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2024-03-27 15:31:57 -0400

    quick fix

--- src/app/employerProfile/page.js
+++ src/app/employerProfile/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Head from 'next/head';
+import Image from "next/image";
 import Link from "next/link";
 import logo from "../../../public/images/logo-no-bg.png";
 import {LoggedInUserProfileNav} from "@/components/loggedInUserProfileNav";
